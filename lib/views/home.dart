5ddfef722b079470e944e361137b515ef92c893d
Chris Wong <wc38.wong@gmail.com>
2023-06-16 10:50:28 +0800

    Simplify the child

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -8,39 +8,35 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Welcome To Flutter")),
       body: Center(
-        child: _buildLists(context),
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('Time'),
+              onTap: () => Navigator.pushNamed(context, '/time'),
+            ),
+            ListTile(
+              title: const Text('Text'),
+              onTap: () => Navigator.pushNamed(context, '/text'),
+            ),
+            ListTile(
+              title: const Text('Image'),
+              onTap: () => Navigator.pushNamed(context, '/image'),
+            ),
+            ListTile(
+              title: const Text('Button'),
+              onTap: () => Navigator.pushNamed(context, '/button'),
+            ),
+            ListTile(
+              title: const Text('Form'),
+              onTap: () => Navigator.pushNamed(context, '/form'),
+            ),
+            ListTile(
+              title: const Text('DatetimePicker'),
+              onTap: () => Navigator.pushNamed(context, '/datetimepicker'),
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-  Widget _buildLists(BuildContext context) {
-    List<Widget> items = [
-      ListTile(
-        title: const Text('Time'),
-        onTap: () => Navigator.pushNamed(context, '/time'),
-      ),
-      ListTile(
-        title: const Text('Text'),
-        onTap: () => Navigator.pushNamed(context, '/text'),
-      ),
-      ListTile(
-        title: const Text('Image'),
-        onTap: () => Navigator.pushNamed(context, '/image'),
-      ),
-      ListTile(
-        title: const Text('Button'),
-        onTap: () => Navigator.pushNamed(context, '/button'),
-      ),
-      ListTile(
-        title: const Text('Form'),
-        onTap: () => Navigator.pushNamed(context, '/form'),
-      ),
-      ListTile(
-        title: const Text('DatetimePicker'),
-        onTap: () => Navigator.pushNamed(context, '/datetimepicker'),
-      ),
-    ];
-
-    return ListView(children: items);
   }
 }
